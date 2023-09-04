1d97db714b8fd690896c87f870533070c0345d94
smart-life-tech <christlightworld@gmail.com>
2023-09-04 19:50:39 +0100

    modified:   display/display.ino 	modified:   readme

--- display/display.ino
+++ display/display.ino
@@ -2,8 +2,8 @@
 #include <Adafruit_ST7735.h>
 
 #define TFT_CS 10    // Chip select line for TFT display
-#define TFT_RST 9    // Reset line for TFT (or connect to +3.3V)
-#define TFT_DC 8     // Data/Command line for TFT
+#define TFT_RST 8    // Reset line for TFT (or connect to +3.3V)
+#define TFT_DC 7     // Data/Command line for TFT
 
 // Initialize the TFT display
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
