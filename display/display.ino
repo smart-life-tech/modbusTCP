3df201bf985e45fa0bab66f897807eb2a230011f
smart-life-tech <christlightworld@gmail.com>
2023-09-05 16:06:30 +0100

    modified:   display/display.ino

--- display/display.ino
+++ display/display.ino
@@ -1,16 +1,17 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 
-#define TFT_CS 10    // Chip select line for TFT display
-#define TFT_RST 8    // Reset line for TFT (or connect to +3.3V)
-#define TFT_DC 7     // Data/Command line for TFT
+#define TFT_CS 10 // Chip select line for TFT display
+#define TFT_RST 8 // Reset line for TFT (or connect to +3.3V)
+#define TFT_DC 7  // Data/Command line for TFT
 
 // Initialize the TFT display
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-void setup() {
+void setup()
+{
   tft.initR(INITR_BLACKTAB); // Initialize ST7735S display
-
+  tft.setRotation(3);
   // Set text color and size
   tft.setTextColor(ST7735_WHITE);
   tft.setTextSize(2);
@@ -19,9 +20,10 @@ void setup() {
   tft.fillScreen(ST7735_BLACK);
 }
 
-void drawLoadBar(int section, int load) {
+void drawLoadBar(int section, int load)
+{
   int x = 0;
-  int y = section * (tft.height() / 3); // Divide the screen into three equal sections
+  int y = section * (tft.height() / 3);             // Divide the screen into three equal sections
   int barWidth = map(load, 0, 200, 0, tft.width()); // Map the load to the display width
 
   // Clear the section
@@ -36,17 +38,23 @@ void drawLoadBar(int section, int load) {
   tft.print("X 32°");
 
   // Draw load bar
-  if (load < 100) {
+  if (load < 100)
+  {
     tft.fillRect(x, y + 100, barWidth, 10, ST7735_GREEN);
-  } else if (load >= 100 && load < 200) {
+  }
+  else if (load >= 100 && load < 200)
+  {
     tft.fillRect(x, y + 100, barWidth, 10, ST7735_YELLOW);
-  } else {
+  }
+  else
+  {
     tft.fillRect(x, y + 100, tft.width(), 10, ST7735_YELLOW);
     tft.fillRect(x + barWidth, y + 100, tft.width() - barWidth, 10, ST7735_RED);
   }
 }
 
-void loop() {
+void loop()
+{
   // Sample load values for each section (you can replace these with actual load values)
   int loadS = 85;
   int loadZ = 128;
@@ -58,5 +66,5 @@ void loop() {
   drawLoadBar(2, loadX);
 
   // Delay or update load values as needed
-  delay(1000); // Adjust the delay time as needed for your application
+  delay(30); // Adjust the delay time as needed for your application
 }
