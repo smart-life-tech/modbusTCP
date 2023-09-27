b7079c825f99b28b302a70e8d69c67b83eca4f2d
smart-life-tech <christlightworld@gmail.com>
2023-09-27 07:22:22 +0100

    new file:   rtuMaster/rtuMaster.ino

--- rtu/rtu.ino
+++ rtu/rtu.ino
@@ -1,36 +1,33 @@
-#include "ModbusRTU.h"//https://github.com/norgor/ModbusRTU/tree/master
+#include "ModbusRTU.h" //https://github.com/norgor/ModbusRTU/tree/master
 
-ModbusRTU::ModbusRTUSlave<16> slave;
+ModbusRTU::ModbusRTUSlave<16> master;
 
-unsigned short encoder = 0;
 unsigned short analogOut[3];
-bool digitalOut[4];
-bool digitalIn[6];
 unsigned short analogIn[2];
 
 void setup()
 {
-    slave.begin(115200);
-    slave.addInputRegister(&analogIn[0], 0x2300);// id aadress changing p20_00
-    slave.update();
+    master.begin(9600);
+    master.addInputRegister(&analogIn[0], 0x2300); // id aadress changing p20_00
+    master.update();
 
-    slave.addHoldingRegister(&analogOut[0], 0x2228);//spindle
-    slave.addHoldingRegister(&analogOut[1], 0x1008);// z axis
-    slave.addHoldingRegister(&analogOut[2], 0x3002);// y axis(not sure yet)
+    master.addHoldingRegister(&analogOut[0], 0x2228); // spindle
+    master.addHoldingRegister(&analogOut[1], 0x1008); // z axis
+    master.addHoldingRegister(&analogOut[2], 0x3002); // y axis(not sure yet)
 }
 
 void loop()
 {
     analogIn[0] = 1;
-    slave.update();
+    master.update();
     Serial.print("current of spinle ");
     Serial.println(analogOut[0]);
     analogIn[0] = 2;
-    slave.update();
+    master.update();
     Serial.print("current of z ");
     Serial.println(analogOut[1]);
     analogIn[0] = 3;
-    slave.update();
+    master.update();
     Serial.print("current of x ");
     Serial.println(analogOut[2]);
     delay(1000);
